6d963374bfea976ccb4cb7e95d0136290bfade98
alramdein <ramdanialif26@gmail.com>
2020-09-02 21:06:36 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Main from './Main';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Main />,
   document.getElementById('root')
 );
 
