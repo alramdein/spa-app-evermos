bb3cdd6e27cacfe40da4927128b9d63f79d54104
alramdein <ramdanialif26@gmail.com>
2020-09-16 08:06:58 +0700

    adding sorting feature

--- src/pages/Filmography.js
+++ src/pages/Filmography.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 import {
   Route,
@@ -9,37 +8,15 @@ import {
 import FilmographyDetail from "../components/templates/FilmographyDetail"
 import FilmographyList from "../components/templates/FilmographyList"
 
-import moviePoster from "../assets/no-img.png"
-
-
 class Filmography extends Component {
-  state = {
-    movies: []
-  }
-  async componentDidMount(){
-    const apiUrl = 'https://ghibliapi.herokuapp.com/films'
-    try {
-      const resp = await axios.get(apiUrl)
-      const movies = resp.data
-      this.setState({
-        movies: movies
-      })
-    } catch(err) {
-      console.error(err)
-    }
-  }
-
   render() {
     return (
         <HashRouter>
             <Route exact path="/filmography">
-                <FilmographyList movies={this.state.movies}/>
+                <FilmographyList/>
             </Route>
             <Route exact path="/filmography/detail">
-              <FilmographyDetail 
-                selectedMovie={this.state.selectedMovie}
-                moviePoster={moviePoster}
-              />
+              <FilmographyDetail />
             </Route>
         </HashRouter>
     );
